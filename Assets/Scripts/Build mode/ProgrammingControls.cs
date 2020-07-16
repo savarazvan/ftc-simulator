41e58192c5ddfe173554da37ca391a33980c8394
savarazvan <razvansava15@yahoo.com>
2020-07-16 13:27:09 +0100

    Autonomous now available in live mode

--- Assets/Scripts/Build mode/ProgrammingControls.cs
+++ Assets/Scripts/Build mode/ProgrammingControls.cs
@@ -4,19 +4,27 @@ using System;
 public class ProgrammingControls : MonoBehaviour
 {
     public Canvas programmingUI, buildUI;
-
-    private void Start() {
+    public bool cursorLocked, liveMode;
+    private void Start()
+    {
         UIDrag.separator = programmingUI.transform.Find
             ("Separator").GetComponent<RectTransform>().localPosition.x;
-        programmingUI.enabled=false;
-        buildUI.enabled=true;    
+        programmingUI.enabled = false;
+        buildUI.enabled = true;
     }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
+            if (liveMode)
+            {
+                Cursor.lockState = cursorLocked ? CursorLockMode.None : CursorLockMode.Locked;
+                Cursor.visible = !cursorLocked;
+                cursorLocked = !cursorLocked;
+            }
             programmingUI.enabled = !programmingUI.enabled;
-            buildUI.enabled = !buildUI.enabled;
+            if (buildUI != null)
+                buildUI.enabled = !buildUI.enabled;
         }
     }
 }
